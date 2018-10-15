e5f93449def05a7b3e844a025561cbe895046fb5
AkramHB <k.e.m.o2008@hotmail.com>
2018-10-15 11:50:32 +0300

    add

--- nomination.php
+++ nomination.php
@@ -224,4 +224,16 @@ mysqli_close($link);
 
 </body>
 
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110676516-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-110676516-1');
+</script>
+
 </html>
